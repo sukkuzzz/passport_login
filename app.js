c8b04666fbf063a5aebd16931f92a33134970112
kym <kym@localhost.localdomain>
2020-12-04 00:32:45 +0300

    change server log message

--- app.js
+++ app.js
@@ -59,4 +59,4 @@ app.use('/users', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
